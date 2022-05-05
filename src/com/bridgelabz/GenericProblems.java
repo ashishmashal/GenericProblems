a0290104f68d12782423dcd1760c609413d53306
ashishmashal <ashishmashal5@gmail.com>
2022-05-05 12:05:37 +0530

    Added:Max From Three String Value Module Is Added

--- src/com/bridgelabz/GenericProblems.java
+++ src/com/bridgelabz/GenericProblems.java
@@ -11,7 +11,14 @@ public class GenericProblems {
      * @param a3 : Third value to compare
      * @return max : Maximum of three values
      */
-
+    public static void maxOfValues(String str1, String str2, String str3){
+        String max = str1;
+        if (str2.compareTo(max)>0)
+            max = str2;
+        if (str3.compareTo(max)>0)
+            max = str3;
+        System.out.println("Maximum of "+str1+", "+str2+", "+str3+" is "+max);
+    }
     public float maxOfValues(float a1, float a2, float a3) {
         float max = a1;
         if (a2 > max)
@@ -34,6 +41,7 @@ public class GenericProblems {
         GenericProblems generics = new GenericProblems();
         System.out.println("The maximum value between the three Number is : " + generics.maxOfValues(8, 15, 5));
         System.out.println("The maximum value between the three Number is : " + generics.maxOfValues(8.0f, 15.1f, 5.8f));
+        generics.maxOfValues("Apple", "Peach", "Banana");
 
     }
 }
