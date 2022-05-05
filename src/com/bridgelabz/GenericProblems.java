07423a3229e2c60830ea3522cd99f37a19494b53
ashishmashal <ashishmashal5@gmail.com>
2022-05-05 12:10:23 +0530

    Modified:Method With Diff Wrapper Class are Replaced With Generic Method

--- src/com/bridgelabz/GenericProblems.java
+++ src/com/bridgelabz/GenericProblems.java
@@ -1,7 +1,7 @@
 package com.bridgelabz;
 /**
- * Purpose - Given 3 Float find the maximum
- * @author -Ashish MAshal
+ * Purpose - Refactor all the 3 to One Generic Method and find the maximum
+ * @author -Ashish Mashal
  */
 public class GenericProblems {
 
@@ -11,29 +11,15 @@ public class GenericProblems {
      * @param a3 : Third value to compare
      * @return max : Maximum of three values
      */
-    public static void maxOfValues(String str1, String str2, String str3){
-        String max = str1;
-        if (str2.compareTo(max)>0)
-            max = str2;
-        if (str3.compareTo(max)>0)
-            max = str3;
-        System.out.println("Maximum of "+str1+", "+str2+", "+str3+" is "+max);
-    }
-    public float maxOfValues(float a1, float a2, float a3) {
-        float max = a1;
-        if (a2 > max)
+    public static <T extends Comparable<T>> T maxOfValues(T a1, T a2, T a3) {
+        T max = a1;
+        if (a2.compareTo(max) > 0)
             max = a2;
-        if (a3 > max)
+        if (a3.compareTo(max) > 0)
             max = a3;
         return max;
-    }
-    public int maxOfValues(int a1, int a2, int a3) {
-        int max = a1;
-        if (a2 > max)
-            max = a2;
-        if (a3 > max)
-            max = a3;
-        return max;
+
+
     }
 
     public static void main(String[] args) {
@@ -41,7 +27,7 @@ public class GenericProblems {
         GenericProblems generics = new GenericProblems();
         System.out.println("The maximum value between the three Number is : " + generics.maxOfValues(8, 15, 5));
         System.out.println("The maximum value between the three Number is : " + generics.maxOfValues(8.0f, 15.1f, 5.8f));
-        generics.maxOfValues("Apple", "Peach", "Banana");
+        System.out.println("The maximum value between the three Strings is : "+ maxOfValues("Lion", "Yak", "zebra"));
 
     }
 }
