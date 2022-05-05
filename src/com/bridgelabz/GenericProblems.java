23cd8064e0b75f72600b8cf1b5a753976e134598
ashishmashal <ashishmashal5@gmail.com>
2022-05-05 11:27:38 +0530

    Added: Maximun From Three Integer Value Module Added

--- src/com/bridgelabz/GenericProblems.java
+++ src/com/bridgelabz/GenericProblems.java
@@ -1,7 +1,19 @@
 package com.bridgelabz;
 
 public class GenericProblems {
+    public int maxOfValues(int a1, int a2, int a3) {
+        int max = a1;
+        if (a2 > max)
+            max = a2;
+        if (a3 > max)
+            max = a3;
+        return max;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome To Generic Problems");
+        GenericProblems generics = new GenericProblems();
+        System.out.println("The maximum value between the three Number is : " + generics.maxOfValues(8, 15, 5));
+
     }
 }
